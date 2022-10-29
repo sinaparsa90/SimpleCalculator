3f8d34a48b1f7ed39943bccbacb6915664aa2af1
sinaparsa90 <82335614+sinaparsa90@users.noreply.github.com>
2022-10-29 05:32:15 +0330

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import CalculatorBox from './CalculatorBox'
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <CalculatorBox />
+    </div>
+  );
+}
+
+export default App;
